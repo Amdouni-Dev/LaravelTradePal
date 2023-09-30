a4486fc36db6c89bf59cee1583d9c920e6ea9824
Yassmin Mouaddeb <98842040+ElMeddebYassmin@users.noreply.github.com>
2023-09-30 13:06:34 +0100

    User/Hazelnut's Factories/Seeders

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,13 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('profile_picture')->nullable();
+            $table->string('account_status')->default('active');
+            $table->string('role')->default('user');
+            $table->text('bio')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
